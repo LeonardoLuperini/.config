7db47627f03380a4967bbe772d3948aa3ab03269
LeonardoLuperini <leonardo.luperini.29@gmail.com>
2022-11-05 19:17:54 +0100

    Ho reso più semplici gli script per aggiornare e scaricare i plugin

--- zsh/setup-zsh.zsh
+++ zsh/setup-zsh.zsh
@@ -1,22 +1,13 @@
 #! /bin/zsh
 
-iferror () {
-	if [[ $1 ]]; then
-		echo "$2 è già stato scaricato"
-	fi
-}
+mkdir plugins
 
-mkdir plugins 2> /dev/null
 cd plugins
 
-git clone https://github.com/zsh-users/zsh-autosuggestions.git 2> /dev/null
-iferror $? "zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git 2> /dev/null
-iferror $? "\nzsh-syntax-highlighting" 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
-git clone https://github.com/MichaelAquilina/zsh-you-should-use.git 2> /dev/null
-iferror $? "\nzsh-you-should-use" 
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git
 
-git clone https://github.com/zsh-users/zsh-history-substring-search.git 2> /dev/null
-iferror $? "\nzsh-history-substring-search" 
+git clone https://github.com/zsh-users/zsh-history-substring-search.git
