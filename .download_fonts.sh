a62710d3e118a87b18796138929035ae97567610
LeonardoLuperini <leonardo.luperini.29@gmail.com>
2023-03-25 01:21:54 +0100

    Modified .download_fonts.sh

--- .download_fonts.sh
+++ .download_fonts.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 cd ../.local/share/fonts 
 
 # Bold
@@ -12,3 +13,49 @@ wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/JetBrains
 # Regular
 wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/JetBrainsMono/Ligatures/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete.ttf?raw=true
 
+cd ~
+
+set -e
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+echo "Setting up Noto Emoji font..."
+# 1 - install  noto-fonts-emoji package
+pacman -S noto-fonts-emoji powerline-fonts --needed
+# 2 - add font config to /etc/fonts/local.conf
+echo "<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+ <alias>
+   <family>sans-serif</family>
+   <prefer>
+     <family>Noto Sans</family>
+     <family>Noto Color Emoji</family>
+     <family>Noto Emoji</family>
+     <family>DejaVu Sans</family>
+   </prefer> 
+ </alias>
+
+ <alias>
+   <family>serif</family>
+   <prefer>
+     <family>Noto Serif</family>
+     <family>Noto Color Emoji</family>
+     <family>Noto Emoji</family>
+     <family>DejaVu Serif</family>
+   </prefer>
+ </alias>
+
+ <alias>
+  <family>monospace</family>
+  <prefer>
+    <family>Noto Mono</family>
+    <family>Noto Color Emoji</family>
+    <family>Noto Emoji</family>
+    <family>DejaVu Sans Mono</family>
+   </prefer>
+ </alias>
+</fontconfig>
+
+" > /etc/fonts/local.conf
+# 3 - update font cache via fc-cache
+fc-cache
+echo "Noto Emoji Font installed! You may need to restart applications like chrome. If chrome displays no symbols or no letters, your default font contains emojis."
